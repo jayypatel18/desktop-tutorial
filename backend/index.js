5639efbe506b9187d663ccba1479b90756936aaf
Jay Patel <jayrpatel1802@gmail.com>
2024-06-12 15:57:52 +0530

    fic

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,9 @@ app.use(cors());
 
 mongoose.connect('mongodb+srv://jayrpatel1802:jay789@cluster0.oadudgd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
-app.get("/",(req,res)=>{
-    res.json("Hello");
-})
+// app.get("/",(req,res)=>{
+//     res.json("Hello");
+// })
 
 
 app.post('/register', (req, res)=>{
